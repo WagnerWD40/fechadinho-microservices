6e85528d8823706d362ee5722edf0cf009b6892b
WagnerWD40 <wagnerwd40@gmail.com>
2024-09-28 10:45:09 -0300

    added auth server

---
+++ fechadinho-gateway/src/test/java/edu/infnet/fechadinho_gateway/FechadinhoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.fechadinho_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FechadinhoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
